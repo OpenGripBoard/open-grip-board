adefed5f46da10dca9fc40564550c82ae56b16e9
Jonas Burkhard <jonas@burkhard.one>
2026-01-05 20:17:26 +0100

    fix: adjust services to new repos

--- backend/src/services/climber_service.rs
+++ backend/src/services/climber_service.rs
@@ -1,9 +1,10 @@
-use crate::{commands::create_climber::CreateClimber, errors::errors::RepositoryError, repositories::climber_repo, structs::climber::Climber};
+use sea_orm::DatabaseConnection;
 
-pub async fn create_climber(cmd: CreateClimber) -> Result<Climber,RepositoryError>{
-    climber_repo::create_climber(cmd).await
-}
+use crate::{errors::errors::RepositoryError, repositories::{climber_repo::ClimberRepo, crud_repo::CrudRepo}, structs::climber::Climber};
 
 pub async fn get_climber(climber_id: i32) -> Result<Climber, RepositoryError>{
-    climber_repo::get_climber(climber_id).await
+    let db : DatabaseConnection = sea_orm::Database::connect("postgres://user:pass@localhost/db").await?;
+    let climber_repo: ClimberRepo = ClimberRepo::new(db);
+    let climber_option = climber_repo.find_by_id(climber_id).await?;
+    climber_option.ok_or(RepositoryError::NotFound)
 }
