01348f2dfe4e1a2bb82777f5fc30dff955b1e418
Jonas Burkhard <jonas@burkhard.one>
2026-01-06 18:26:17 +0100

    feat: add insert method to repos

--- backend/src/repositories/climber_repo.rs
+++ backend/src/repositories/climber_repo.rs
@@ -1,6 +1,7 @@
 use entity::climbers;
 use rocket::async_trait;
-use sea_orm::{DatabaseConnection, DeleteResult, EntityTrait};
+use sea_orm::{ActiveModelTrait, DatabaseConnection, DeleteResult, EntityTrait, Set};
+use crate::commands::create_climber::CreateClimber;
 use crate::repositories::crud_repo::CrudRepo;
 use crate::structs::climber::Climber;
 use crate::errors::errors::RepositoryError;
@@ -16,7 +17,7 @@ impl ClimberRepo {
 }
 
 #[async_trait]
-impl CrudRepo<Climber, i32> for ClimberRepo{
+impl CrudRepo<Climber, CreateClimber, i32> for ClimberRepo{
     async fn find_by_id(&self, id: i32)-> Result<Option<Climber>, RepositoryError>{
         let climber_model = climbers::Entity::find_by_id(id)
             .one(&self.db)
@@ -33,5 +34,17 @@ impl CrudRepo<Climber, i32> for ClimberRepo{
         } else {
             Err(RepositoryError::NotFound)
         }
+    }
+
+    async fn insert(&self, new_climber: CreateClimber) -> Result<Climber, RepositoryError>{
+        let climber = climbers::ActiveModel {
+            username: Set(new_climber.username),
+            email: Set(new_climber.email),
+            password_hash: Set("todo!()".to_string()),
+            profile_pic_id: Set(0),
+            ..Default::default()
+        };
+        let climber_model: climbers::Model = climber.insert(&self.db).await?;
+        Ok(Climber::from(climber_model))
     }
 }
