03f0310112d0cc47f81007c267f4550d48a70861
Jonas Burkhard <jonas@burkhard.one>
2026-01-11 18:56:58 +0100

    feat: add record service

---
+++ backend/src/services/record_service.rs
@@ -0,0 +1,11 @@
+use crate::{commands::create_record::CreateRecord, errors::errors::RepositoryError, repositories::{crud_repo::CrudRepo, record_repo::RecordRepo}, structs::record::Record};
+
+pub struct RecordService{
+    repo: RecordRepo,
+}
+
+impl RecordService {
+    pub async fn create_record(&self, new_record: CreateRecord)-> Result<Record, RepositoryError> {
+        self.repo.insert(new_record).await
+    }
+}
