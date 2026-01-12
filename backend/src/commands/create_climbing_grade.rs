7942cf191bcd6283e4e7ce8983efa0fbfdee6610
Jonas Burkhard <jonas@burkhard.one>
2026-01-12 22:45:18 +0100

    style: apply rust formatting

--- backend/src/commands/create_climbing_grade.rs
+++ backend/src/commands/create_climbing_grade.rs
@@ -1,6 +1,6 @@
 use crate::dto::request::new_climbing_grade_dto::NewClimbingGradeDto;
 
-pub struct CreateClimbingGrade{
+pub struct CreateClimbingGrade {
     pub name: String,
     pub grade_context: String,
     pub numerical_value: i32,
@@ -8,7 +8,7 @@ pub struct CreateClimbingGrade{
 
 impl From<NewClimbingGradeDto> for CreateClimbingGrade {
     fn from(grade: NewClimbingGradeDto) -> Self {
-        CreateClimbingGrade { 
+        CreateClimbingGrade {
             name: grade.name,
             grade_context: grade.grade_context,
             numerical_value: grade.numerical_value,
